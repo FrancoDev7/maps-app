9d2894c6bc7bd03c589b2a7348132ba2f24f47c6
FrancoDev7 <francisco.pereiras@correoaiep.cl>
2026-01-22 17:01:58 -0300

    Agregar funcionalidad de geolocalización y manejar errores en PlacesProvider

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { MapsApp } from "./MapsApp";
 
+// Verificar si el navegador soporta la API de Geolocalización
+if (!navigator.geolocation) {
+  alert("Tu navegador no soporta Geolocalización");
+  throw new Error("Tu navegador no soporta Geolocalización");
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
