a2514516867230a626dbfdf83a4d27a109df26ff
FrancoDev7 <francisco.pereiras@correoaiep.cl>
2026-01-22 18:14:40 -0300

    Agregar soporte para Mapbox, incluyendo MapProvider y MapView, y actualizar HomeScreen para incluir BtnMyLocation

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { MapsApp } from "./MapsApp";
 
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZnJhbmNvZGV2NyIsImEiOiJjbWtwd25zdnUwbDR4M2VwcWdieXdlaHgwIn0.P1JWCViZ2S69lkHcSyGi0w";
+
 // Verificar si el navegador soporta la API de Geolocalización
 if (!navigator.geolocation) {
   alert("Tu navegador no soporta Geolocalización");
